313be8346553adcb6831c176b86d906bf1943dd5
Daniel Guzman <danielgmnr@gmail.com>
2026-05-08 10:03:17 -0500

    First commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const {createServer} = require('node:http');
+const {Server} = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('Alguien se conectó');
+    // Mensajes a todos
+    socket.emit('bienvenida', 'Holi UwU <3');
+
+    socket.on('mensaje', (mensaje) => {
+        console.log(mensaje);
+    
+    });
+});
+
+
+server.listen(3000, () => {
+    console.log('Corriendo en el puerto 3000');
+});
