90b6a8c637eba2509e33fe7e8ddc005dc9943385
Daniel Guzman <danielgmnr@gmail.com>
2026-05-11 08:58:54 -0500

    Cambios visuales

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
+
 function App(){
+  const [inputMessage, setInputMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Como se envían los mensajes...
+
+  }
+
+
   return (
     <div>
-      <form>
-        <input/>
-        <button>Enviar</button>
+      <form onSubmit={handleSubmit}>
+        <input onChange={(e) => setInputMessage(e.target.value)}/>
+        <button type="submit">Enviar</button>
       </form>
     </div>
   );
